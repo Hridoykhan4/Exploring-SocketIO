19749a80245b5d0db3d3085baf9e0ed24f5370ce
Hridoykhan4 <hridoykhan148385@gmail.com>
2026-05-23 23:52:41 +0600

    status updated

---
+++ SocketIO_Playlist/config/database.js
@@ -0,0 +1,55 @@
+import { MongoClient } from 'mongodb';
+
+let client = null;
+let db = null;
+
+/**
+ * Connect to MongoDB Atlas
+ */
+export const connectDB = async () => {
+  try {
+  
+    client = new MongoClient(process.env.MONGODB_URI);
+
+    await client.connect();
+
+    db = client.db();
+    
+    console.log('✅ MongoDB Connected Successfully');
+    console.log(`📊 Database: ${db.databaseName}`);
+    
+    return db;
+  } catch (error) {
+    console.log(error)
+    console.error('❌ MongoDB Connection Error:', error.message);
+    process.exit(1);
+  }
+};
+
+/**
+ * Get database instance
+ */
+export const getDB = () => {
+  if (!db) {
+    throw new Error('Database not initialized. Call connectDB() first!');
+  }
+  return db;
+};
+
+/**
+ * Get collection
+ */
+export const getCollection = (collectionName) => {
+  return getDB().collection(collectionName);
+};
+
+/**
+ * Close database connection
+ */
+export const closeDB = async () => {
+  if (client) {
+    await client.close();
+    console.log('🔒 MongoDB connection closed');
+  }
+};
+
